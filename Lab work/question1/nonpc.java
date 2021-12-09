bdd7de8a71387b0ac863740ee1cbe222ace025f9
swachitta <59694204+swachitta@users.noreply.github.com>
2021-12-09 08:53:18 +0545

    Add files via upload

---
+++ Lab work/question1/nonpc.java
@@ -0,0 +1,17 @@
+class Main {
+
+  String name;
+
+ 
+  private Main() {
+    name = " my name is aashish";
+ 
+  }
+
+  public static void main(String[] args) {
+
+    
+    Main obj = new Main();
+    System.out.println( obj.name);
+  }
+}
---
+++ Lab work/question1/pcon.java
@@ -0,0 +1,22 @@
+
+// parameterizeddd
+class Pcon {
+
+  String fname;
+
+ 
+   Pcon(String name) {
+   name = "aashish";
+    fname = name;
+    System.out.println(fname + "aashish");
+    
+ 
+  }
+
+  public static void main(String[] args) {
+
+    
+    Pcon obj = new Pcon("aashish");
+
+  }
+}
